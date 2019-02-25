42fb976c8896cf8fa8858ddae627936830a09ace
Adam Pearson <46682455+AD4M5K11@users.noreply.github.com>
2019-02-25 18:24:02 +0000

    Add files via upload
    first upload (5 days progress beforehand)

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+const supra = document.querySelector('.sound')
+
+    supra.addEventListener('click', (event) => {
+        
+    const supraAudio = document.querySelector ('.supra')
+    supraAudio.play()
+    supraAudio.currentTime = 0
+
+    });
+
+   const door = document.querySelector(".open")
+
+
+
+   
+
+
+
+
