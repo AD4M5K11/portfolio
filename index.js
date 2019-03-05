3a413e85978ca87ff7a285fdecfa7fd8929d5e54
Adam Pearson <46682455+AD4M5K11@users.noreply.github.com>
2019-03-05 10:00:36 +0000

    new file names

---
+++ index.js
@@ -0,0 +1,22 @@
+const supra = document.querySelector('.sound')
+
+    supra.addEventListener('click', (event) => {
+        
+    const supraAudio = document.querySelector ('.supra')
+    supraAudio.play()
+    supraAudio.currentTime = 0
+
+    });
+
+   const door = document.querySelector(".open");
+
+
+
+
+
+
+   
+
+
+
+
