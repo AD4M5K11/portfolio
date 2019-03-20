c9df320ae81563f2828a588234bf9588d3a48a5c
Adam Pearson <46682455+AD4M5K11@users.noreply.github.com>
2019-03-20 15:03:02 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -11,10 +11,3 @@ const supra = document.querySelector('.sound')
     supraAudio.currentTime = 0
 
     });
-
-    let showAboutMe = document.querySelector('.aboutme')
-    function function1() {
-       showAboutMe.classList.toggle('.aboutme')
-    };
-      
-
